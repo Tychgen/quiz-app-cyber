e5e1049e8affb5d6e0c7472fc332bdffb19a648e
Tychgen <alikxelefov@gmail.com>
2023-10-04 11:20:25 +0400

    Completed QuestionCounter component

--- src/components/QuestionCounter.jsx
+++ src/components/QuestionCounter.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { QuizContext } from '../../hooks/context/QuizContext';
 
 const QuestionCounter = () => {
+  const {currentQuestion, questions} =  useContext(QuizContext)
   return (
     <div>
-      
+      {`${currentQuestion + 1} of ${questions.length} Questions`}
     </div>
-  )
-}
+  );
+};
 
-export default QuestionCounter
+export default QuestionCounter;
