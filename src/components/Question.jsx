234c099d22597f055e752fb67b6d5b23e37c9dee
Tychgen <alikxelefov@gmail.com>
2023-10-04 11:21:23 +0400

    Completed Question component

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import { useContext } from "react";
+import { QuizContext } from "../../hooks/context/QuizContext";
 
 const Question = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const {
+    currentQuestion,
+    userAnswers,
+    showAnswers,
+    questions,
+    handleAnswerClick
+  } = useContext(QuizContext);
 
-export default Question
+  const question = questions[currentQuestion];
+
+  
+
+  return (
+    <>
+      <div className='question'>
+        <span>{question.questionText}</span>
+      </div>
+      <div className='option-list'>
+        {question.answerOptions.map((answer, index) => {
+          const isSelected = userAnswers[currentQuestion] === answer;
+          const isCorrect = question.correctAnswer === answer;
+          const isIncorrect = isSelected && !isCorrect;
+
+          return (
+            <div
+              key={index}
+              onClick={() => handleAnswerClick(answer)}
+              className={`option disabled ${
+                showAnswers
+                  ? isCorrect
+                    ? 'correct'
+                    : isSelected
+                    ? 'incorrect'
+                    : ''
+                  : isSelected
+                  ? 'highlighted'
+                  : ''
+              }`}
+            >
+              {answer}
+              <span></span>
+              {(isCorrect || isIncorrect) && showAnswers && (
+                <div className={`icon ${isCorrect ? 'tick' : 'wrong'}`}>
+                  <i className={`fas fa-${isCorrect ? 'check' : 'times'}`}></i>
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Question;
